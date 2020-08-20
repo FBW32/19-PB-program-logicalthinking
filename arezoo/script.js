d18772f342c518c01f5d5a1f34c27969cddd5cfe
arezoofaraji <arezoo.faraji66@gmail.com>
2020-08-20 15:06:54 +0200

    19-done

---
+++ arezoo/script.js
@@ -0,0 +1,24 @@
+let numOne = "a";
+let numTwo = "b";
+if (numOne > numTwo) {
+  console.log(numOne);
+} else {
+  console.log(numTwo);
+}
+
+let var1 = "c";
+let var2 = "d";
+
+if (var1 == var2) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let number1 = "f";
+let number2 = "a";
+if (number1 > number2) {
+  console.log(true);
+} else {
+  console.log(false);
+}
