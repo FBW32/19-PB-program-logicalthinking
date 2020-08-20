48a7fac357aa5cf798226520c935bbd3f209d1a6
Gabriele Pepe <gabriele.pepe@gmail.com>
2020-08-20 14:29:46 +0200

    task completed

---
+++ Gabriele/logicalThinking.js
@@ -0,0 +1,30 @@
+// Write down pseudo-code on pen and paper for the following patterns:
+
+
+// 1.
+
+let a = "num1";
+let b = "num2";
+
+if (a > b) {
+    console.log(a);
+} else if (b > a)  {
+    console.log(b);
+};
+
+// 2.
+
+let c = "num3";
+let d = "num4";
+
+if (c === d) {
+    console.log(true);
+} else if (c !== d){
+    console.log(false);
+};
+
+// 3.
+
+let e = 8;   
+let f = 6;   
+(e > f) ? console.log(true) : console.log(false);
