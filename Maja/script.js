e7da8f476ce0dba6013bd687a401ddfff388d382
Maja <majamarek.design@gmail.com>
2020-08-20 15:59:00 +0200

    19 done

---
+++ Maja/script.js
@@ -0,0 +1,12 @@
+// 1.
+console.log("num1", Math.max(2, 5)); //num1 5
+console.log("num2", Math.max(0, 100)); //num2 100
+console.log("num3", Math.max(52, 35)); //num3 52
+// 2.
+console.log("num4", 5 === 5); // num4 true
+console.log("num5", 40 === 40); // num5 true
+console.log("num5", 50 === 51); // num5 false
+// 3.
+console.log("num6", 110 > 100); // num6 true
+console.log("num7", 150 > 100); // num6  true
+console.log("num8", 50 > 100); // num6 false
