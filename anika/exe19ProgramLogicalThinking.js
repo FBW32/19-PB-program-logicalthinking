5b4a1008b1b8864693893537d755b62a53e96d46
anika555 <ak15@live.de>
2020-08-23 09:26:30 +0200

    anika - Exercise 19 Program logical thinking

---
+++ anika/exe19ProgramLogicalThinking.js
@@ -0,0 +1,40 @@
+// ***********EXERCISE 19 PROGRAM LOGICAL THINKING**************
+
+// # Inputs & Outputs
+
+// Write down pseudo-code on pen and paper for the following patterns:
+
+// #### 1.
+
+// | Input  | Output |
+// | :----: | :----: |
+// |  2, 5  |   5    |
+// | 0, 100 |  100   |
+// | 52, 35 |   52   |
+
+// If 5 is greater than 2, print 5.
+// If 100 is greater than 0, print 100.
+// If 52 is greater than 35, print 35.
+// (If one number is greater than other number, print greater number.)
+
+// #### 2.
+
+// | Input  | Output |
+// | :----: | :----: |
+// |  5, 5  |  true  |
+// | 40, 40 |  true  |
+// | 50, 51 | false  |
+
+// If first number is equal to other number, print true.
+// Else, print false.
+
+// #### 3.
+
+// |  Input   | Output |
+// | :------: | :----: |
+// | 110, 100 |  true  |
+// | 150, 100 |  true  |
+// | 50, 100  | false  |
+
+// If first number is greater than 100 (second number), print true.
+// Else, print false.
