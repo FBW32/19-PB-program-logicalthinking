87045c02147e4d5147a05f3d4e6209203bfb4b78
Misbah-D <misbah.deeb@gmail.com>
2020-08-23 19:26:06 +0200

    ok

---
+++ answers.js
@@ -0,0 +1,39 @@
+//-----1--------
+// Input	Output
+// 2, 5	      5
+// 0, 100	  100
+// 52, 35	  52
+let x= 2; //0 //52
+let y= 5; //100 //35
+console.log(Math.max(x, y)
+
+
+//---------2------------
+
+// Input	Output
+// 5, 5	true
+// 40, 40	true
+// 50, 51	false
+    
+ let  x1 = 5 ; // 5, 40, 50
+ let  y1 = 5; // 5, 40, 51
+ if (x1 == y1) {console.log(true);
+    
+ } else { console.log(false);
+    
+ }
+
+ //------------3-------------
+
+ // Input	Output
+// 110, 100	true
+// 150, 100	true
+// 50, 100	false
+    
+ let  x2 = 110 ; // 110, 150, 50
+ let  y2 = 100; // 100, 100, 100
+ if (x2 > y2) {console.log(true);
+    
+ } else { console.log(false);
+    
+ }
